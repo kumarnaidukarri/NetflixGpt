c9461419d596b2dae31807dc3b98d72b1360903a
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-19 01:43:16 +0530

    1.4 - implemented Basic routing using React Router

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+// my components
+import Browse from "./Browse.jsx";
+import Login from "./Login.jsx";
+
 const Body = () => {
-  return <div>Body</div>;
+  const appRouter = createBrowserRouter([
+    { path: "/", element: <Login /> },
+    { path: "/browse", element: <Browse /> },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={appRouter}></RouterProvider>
+    </div>
+  );
 };
 
 export default Body;
