bc15b5772a517658484137d800f23aacf36aaf3e
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-25 00:01:07 +0530

    4 - 'Watch Mode' API call to get movies list

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+
 import Header from "./Header.jsx";
+import { WatchModeAPI__URL } from "../utils/constants.js";
 
 const Browse = () => {
+  // Async method
+  const getNowPlayingMovies = async () => {
+    const responseObj = await fetch(WatchModeAPI__URL);
+    const jsonData = await responseObj.json();
+    // console.log(jsonData);
+
+    const moviesReleasesList = jsonData?.releases; // [m1{},m2{},m3{},m4{},...]
+    console.log(moviesReleasesList);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies(); // API call for movies list
+  }, []);
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,19 @@
-// this file contains all hard-coded urls, strings.
+// this file contains all hard-coded URLs, strings.
+
 const LOGO =
   "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 const USER_AVATAR = "https://cdn-icons-png.flaticon.com/512/10337/10337609.png";
 
-export { LOGO, USER_AVATAR };
+//
+// !!! Strugging to get Free Movies APIs. !!!
+/* 
+Movies API providers:-  TMDB, OMDB, TraktApiary Api, WatchMode 
+TV shows API providers:- TVMaze(https://api.tvmaze.com/shows)
+*/
+// TMDB movie api website not working.
+// 1. WATCHMODE API
+const WatchModeAPI__KEY = "KmUpfDsCw8ErfxvazuB5fpv6pksFInhIy6feFfCQ";
+const WatchModeAPI__URL = `https://api.watchmode.com/v1/releases/?apiKey=${WatchModeAPI__KEY}`; // resObj.releases=[mobj1,mobj2]
+
+export { LOGO, USER_AVATAR, WatchModeAPI__URL };
