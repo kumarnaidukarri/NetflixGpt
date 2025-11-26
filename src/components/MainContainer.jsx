3c871050647ff4d4e92c964d5ceee482a61fc3c0
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-27 00:27:46 +0530

    4.3.2 - Trailer API not working, using static data, state variable to store videoWatchId, youtube embed player

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -13,8 +13,9 @@ const MainContainer = () => {
   if (movies === null) return;
 
   const mainMovie = movies[0];
-  console.log(mainMovie);
+  console.log("main movie:- ", mainMovie);
 
+  /*
   const {
     title,
     id,
@@ -23,7 +24,26 @@ const MainContainer = () => {
     poster_url,
     overview = "This is the overview of the movie ...",
   } = mainMovie;
+  */
 
+  const klausObj = {
+    title: "Klaus",
+    id: "t4729430",
+    imdb_id: "t4729430",
+    tmdb_id: "t4729430",
+    poster_url:
+      "https://www.imdb.com/title/tt4729430/mediaviewer/rm1843825409/?ref_=tt_ov_i",
+    overview:
+      "A simple act of kindness always sparks another, even in a frozen, faraway place. When Smeerensburg's new postman, Jesper, befriends toymaker Klaus, their gifts melt an age-old feud and deliver a sleigh full of holiday traditions",
+  };
+  const {
+    title,
+    id,
+    imdb_id,
+    tmdb_id,
+    poster_url,
+    overview = "This is the overview of the movie ...",
+  } = klausObj;
   return (
     <div>
       <VideoTitle title={title} overview={overview} />
--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,6 +1,6 @@
 // video background of Main Container of Browse Page
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   KINOCHECKAPI__MoviesURL,
   KINOCHECKAPI__HeaderOptions,
@@ -9,21 +9,46 @@ import {
 const VideoBackground = (props) => {
   const { tmdbId } = props;
 
-  // fetch the 'trailer video' by using tmdb_id of movie
-  const getMovieVideos = async () => {
-    const responseObj = await fetch(
-      KINOCHECKAPI__MoviesURL + "299534",
-      KINOCHECKAPI__HeaderOptions
-    ); // "299534"
-    const jsonData = await responseObj.json();
-    console.log(jsonData);
-  };
+  const [watchId, setWatchId] = useState(null); // state variable to store 'Youtube Watch Id'
 
   useEffect(() => {
-    getMovieVideos();
+    // fetch the 'trailer video' by using tmdb_id of movie
+    const getMovieTrailer = async () => {
+      // !!! APIs not working to fetch data by id. so, Using Static hard-coded dummy data trailer.
+      /*
+      const responseObj = await fetch(
+        KINOCHECKAPI__MoviesURL + tmdbId,
+        KINOCHECKAPI__HeaderOptions
+      ); // "299534"
+      const jsonData = await responseObj.json();
+      // console.log( jsonData);
+
+      const trailerObj = jsonData?.trailer;
+      // console.log(trailerObj);
+      // const key = trailerObj.youtube_video_id;
+      */
+
+      // Youtube Embeded Player
+      const watchKey = "taE3PwurhYM";
+      const youtubeWatchLink = "https://www.youtube.com/watch?v=" + watchKey;
+      setWatchId(watchKey); // Updates the state
+    };
+    getMovieTrailer();
   }, []); // runs only on initial first render
 
-  return <div>VideoBackground</div>;
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + watchId}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
