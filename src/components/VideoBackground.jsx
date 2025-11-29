df7e0172f4179a7fa27948132a45f0466ba5edde
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-29 15:34:02 +0530

    4.3.5 - applied stylings to Trailer container

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -12,9 +12,12 @@ const VideoBackground = (props) => {
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailer?.watchKey}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailer?.watchKey +
+          `?autoplay=1&mute=1&loop=1&playlist=${trailer?.watchKey}&controls=0&rel=0&modestbranding=1`
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,11 +4,11 @@ const VideoTitle = (props) => {
   const { title, overview } = props;
 
   return (
-    <div className="title-container  pt-36 px-6">
+    <div className="title-container  pt-36 px-6 absolute text-white bg-linear-to-r from-black aspect-video pt-[20%] px-24">
       <h1 className="text-6xl font-bold">{title} </h1>
       <p className="text-lg py-6 w-1/4"> {overview} </p>
       <div className="buttons-container">
-        <button className="px-9 py-3 text-lg text-white bg-gray-400 opacity-80 rounded-md">
+        <button className="px-9 py-3 text-lg text-black bg-white rounded-md  hover:opacity-80 hover:cursor-pointer">
           ▶️ Play
         </button>
         <button className="mx-2 px-9 py-3 text-lg text-white bg-gray-400 opacity-80 rounded-md">
