f9c26e523c72cb049283f161c29a61dffca6b193
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-30 18:05:03 +0530

    6.3 - Updated Gpt Page component code

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,7 +1,20 @@
 // Gpt search bar component
 
 const GptSearchBar = () => {
-  return <div>GptSearchBar</div>;
+  return (
+    <div className="Gpt-Searchbar  pt-[7.5%]  flex justify-center">
+      <form className="w-1/2 bg-black  grid grid-cols-12">
+        <input
+          type="text"
+          placeholder="What would you like to watch today?"
+          className="m-4 p-4 bg-white rounded-sm  col-span-9"
+        />
+        <button className="px-4 py-2 m-4 text-white bg-red-700 rounded-lg cursor-pointer hover:bg-red-600  col-span-3">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default GptSearchBar;
--- src/components/GptSearchPage.jsx
+++ src/components/GptSearchPage.jsx
@@ -1,10 +1,23 @@
 // Gpt Search Page contains Search bar, Movie suggestions.
 
+import GptSearchBar from "./GptSearchBar";
+import GptMovieSuggestions from "./GptMovieSuggestions";
+
 const GptSearchPage = () => {
   return (
-    <div>
-      {/* Gpt search bar
-          Gpt Movie Suggestions */}
+    <div className="gpt-search-page">
+      {/* background image */}
+      <div className="bg-container absolute -z-10">
+        <img
+          alt="netflix background image"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/e94073b0-a056-402f-9015-16cb1e7e45c2/web/IN-en-20251110-TRIFECTA-perspective_46e74acc-70aa-4691-988a-dbcf958149d1_small.jpg"
+          className="h-screen w-screen"
+        />
+      </div>
+
+      {/* Gpt search bar, Gpt Movie Suggestions */}
+      <GptSearchBar />
+      <GptMovieSuggestions />
     </div>
   );
 };
