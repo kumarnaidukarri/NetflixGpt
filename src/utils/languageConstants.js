7acbd6675e65b64d37fa9cd49bedae02b1a1d519
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-30 20:47:22 +0530

    7 - Implementing Multi-language support using 'Internationalization'

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,28 @@
+// this file contains data for multi-language support.
+
+const lang = {
+  en: {
+    search: "Search",
+    gptSearchPlaceholder: "What would you like to watch today?",
+  },
+  hindi: {
+    search: "खोजें",
+    gptSearchPlaceholder: "आज आप क्या देखना चाहेंगे?",
+  },
+  telugu: {
+    search: "వెతకండి",
+    gptSearchPlaceholder: "ఈ రోజు మీరు ఏమి చూడాలని కోరుకుంటున్నారు?",
+  },
+  spanish: {
+    search: "Buscar",
+    gptSearchPlaceholder: "¿Qué te gustaría ver hoy?",
+  },
+};
+
+export default lang;
+
+// Internationalization(i8n) :-
+/*
+  i18n -  process of making an app to support multiple languages.
+  i10n -  adding translations for each language.  
+*/
