5157b942bd5e23b1c150cedb1b46c4f65c58da01
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-30 21:08:17 +0530

    7.1 - support for GptSearchBar using lang js object

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,16 +1,18 @@
 // Gpt search bar component
 
+import lang from "../utils/languageConstants.js";
+
 const GptSearchBar = () => {
   return (
     <div className="Gpt-Searchbar  pt-[7.5%]  flex justify-center">
       <form className="w-1/2 bg-black  grid grid-cols-12">
         <input
           type="text"
-          placeholder="What would you like to watch today?"
+          placeholder={lang.telugu.gptSearchPlaceholder}
           className="m-4 p-4 bg-white rounded-sm  col-span-9"
         />
         <button className="px-4 py-2 m-4 text-white bg-red-700 rounded-lg cursor-pointer hover:bg-red-600  col-span-3">
-          Search
+          {lang.telugu.search}
         </button>
       </form>
     </div>
