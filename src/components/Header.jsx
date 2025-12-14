29d5682683c46172be4c2e252d001760432692b9
Kumar <kumarnaidukarri2002@gmail.com>
2025-12-14 21:45:24 +0530

    9 - Tailwindcss Responsive Design using breakpoints

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -83,7 +83,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="absolute w-screen z-10 px-8 py-2  bg-gradient-to-bottom from-black  flex justify-between items-center">
+    <div className="absolute w-screen z-10 px-8 py-2  bg-gradient-to-bottom from-black  flex justify-between items-center  bg-black sm:bg-red-500  md:bg-green-500  lg:bg-blue-500  xl:bg-yellow-500  2xl:bg-pink-500">
       <img className="w-44" alt="netflix logo" src={LOGO} />
       {user && (
         <div className="flex gap-2  p-2">
